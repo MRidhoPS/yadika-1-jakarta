71f352581088a92394d2231ffa0bb1df6d356ec5
mridhops <mridhophs@gmail.com>
2026-09-15 21:17:38 +0700

    change: google meta name

--- app/layout.js
+++ app/layout.js
@@ -127,7 +127,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        <meta name="google-site-verification" content="Y8nat2RFjpvaSabCpc2KWf4pQt_BYtta_2efi8WJcf0" />
+        {/* <meta name="google-site-verification" content="Y8nat2RFjpvaSabCpc2KWf4pQt_BYtta_2efi8WJcf0" /> */}
+        <meta name="google-site-verification" content="AI1Nds9K2AQDHReWMV_RPYueAT2BRXy80ZMlEHVmjh4" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
